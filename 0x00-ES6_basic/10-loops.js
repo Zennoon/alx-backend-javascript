f05b74b7bb7c216e10fc758706ff702f9cbab9bb
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-23 00:12:41 +0300

    Use the entries() array method with the for...of loop to append a value to every item in an array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [idx, val] of array.entries()) {
+    array[idx] = appendString + val;
+  }
+
+  return (array);
+}
--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,6 +1,3 @@
-import getFullBudgetObject from './9-getFullBudget.js';
+import appendToEachArrayValue from "./10-loops";
 
-const fullBudget = getFullBudgetObject(20, 50, 10);
-
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
-console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
