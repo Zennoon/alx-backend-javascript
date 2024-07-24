d8eb0f6b92424ad7d82c71ff6e6b189c54c85fa6
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 18:24:42 +0300

    Covert to an async function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])).then((vals) => {
-    const statusArray = [];
-
-    vals.forEach(({status, value, reason}) => {
-      statusArray.push({
-        status,
-        value: value || reason.message,
-      })
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const vals = await (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]));
+  const statusArray = [];
+  vals.forEach(({ status, value, reason }) => {
+    statusArray.push({
+      status,
+      value: value || reason.message,
     });
-    return statusArray;
   });
+  return statusArray;
 }
