ba891300ae202518c1a98cf5ce0ddf5df1eca6ba
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 18:17:11 +0300

    Use sructure from 3-all.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((arr) => {
-      const statusArray = [];
+  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])).then((vals) => {
+    const statusArray = [];
 
-      arr.forEach(({ status, value, reason }) => {
-        statusArray.push({
-          status,
-          value: value || reason.message,
-        });
-      });
-      return (statusArray);
-    })
-  );
+    vals.forEach(({status, value, reason}) => {
+      statusArray.push({
+        status,
+        value: value || reason,
+      })
+    });
+    return statusArray;
+  });
 }
