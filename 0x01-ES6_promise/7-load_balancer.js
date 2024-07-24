5c77705136b041579345b7f2d4467cc39438bc6a
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 18:49:04 +0300

    Fix eslint issues

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default async function loadBalancer(download1, download2) {
-    const fastest = await Promise.race([download1, download2]);
+  const fastest = await Promise.race([download1, download2]);
 
-    return (fastest);
+  return (fastest);
 }
