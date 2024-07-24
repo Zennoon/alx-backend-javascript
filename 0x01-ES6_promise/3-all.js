eee09b368f2d73080a717c8f4dde7ffeb2e06c31
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 17:05:45 +0300

    Make the function return the cumulated promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const allPromises = Promise.all([uploadPhoto(), createUser()]);
-
-  try {
-    allPromises.then((arr) => {
-      console.log(`${arr[0].body} ${arr[1].firstName} ${arr[1].lastName}`);
-    }).catch(() => {
-      console.error('Signup system offline');
-    });
-  } catch (e) {
-    console.error('Signup system offline');
-  }
-  return (allPromises);
+  return (Promise.all([uploadPhoto(), createUser()])).then((vals) => {
+    console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
 }
