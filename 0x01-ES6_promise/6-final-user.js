2a16c3cd59c150abffcdbda79e956f48298c34c3
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 17:57:52 +0300

    Revert to previous implementation with error object returned

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]));
+  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((arr) => {
+      const statusArray = [];
+
+      arr.forEach(({ status, value, reason }) => {
+        statusArray.push({
+          status,
+          value: value || reason,
+        });
+      });
+      return (statusArray);
+    })
+  );
 }
