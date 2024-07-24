517d133b85b81cecf819ccb9589bacfd9003bf13
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 16:36:47 +0300

    Try catching errors with try/catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,14 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const allPromises = Promise.all([uploadPhoto(), createUser()]);
 
-  allPromises.then((arr) => {
-    console.log(`${arr[0].body} ${arr[1].firstName} ${arr[1].lastName}`);
-  }).catch(() => {
+  try {
+    allPromises.then((arr) => {
+      console.log(`${arr[0].body} ${arr[1].firstName} ${arr[1].lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
+  } catch (e) {
     console.log('Signup system offline');
-  });
+  }
   return (allPromises);
 }
