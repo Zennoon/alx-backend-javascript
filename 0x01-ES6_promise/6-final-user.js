65c52079731880247edff8dd902156923f22bed5
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-24 17:51:08 +0300

    Remove property filtering and return fulfillment array as is

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((arr) => {
-      const statusArr = [];
-
-      arr.forEach(({ status, value, reason }) => {
-        statusArr.push({
-          status,
-          value: value || reason.message,
-        });
-      });
-      return (statusArr);
-    })
-  );
+  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]));
 }
