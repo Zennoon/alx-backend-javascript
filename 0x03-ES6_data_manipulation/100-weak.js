39518c5bde409378cf71b439243dd9f298ae819c
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 21:50:05 +0300

    Try throwing the error after tracking the number of requests

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
+  weakMap.set(endpoint, weakMap.get(endpoint) ? weakMap.get(endpoint) + 1 : 1);
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) ? weakMap.get(endpoint) + 1 : 1);
 }
