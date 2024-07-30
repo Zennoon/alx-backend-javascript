7370dce9f403b0c02ccca255588559dd4287fdc1
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 21:10:07 +0300

    Write a function that creates and returns a map from a hardcoded array of key-value pairs

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,4 +1,3 @@
-import cleanSet from "./8-clean_set";
+import groceriesList from "./9-groceries_list";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(groceriesList());
