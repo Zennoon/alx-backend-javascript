0f07a28cb88aa5bc70720e7b34fbafd6a2608de6
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 19:35:24 +0300

    Check if the given set value is not a set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   // Returns a string of all the set value elements that start with a given string
   // with that string removed from each one
-  if (typeof startString !== 'string' || startString.length === 0) {
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   const contains = [];
