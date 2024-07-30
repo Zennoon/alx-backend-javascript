960b2222bd29ad940be0dc7b8b0e6192d469d47e
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 17:53:31 +0300

    Write a function that accepts an array of student objects and uses the reduce method to get a sum of their ids

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,6 +1,6 @@
 import getListStudents from "./0-get_list_students";
-import getStudentsByLocation from "./2-get_students_by_loc";
+import getStudentsIdsSum from "./3-get_ids_sum";
 
 const students = getListStudents();
 
-console.log(getStudentsByLocation(students, 'San Francisco'));
+console.log(getStudentsIdsSum(students));
