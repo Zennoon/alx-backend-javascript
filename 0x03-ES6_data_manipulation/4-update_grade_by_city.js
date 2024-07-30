3a538c17d7cfb6d66094298524252fca281cdf1b
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 18:20:05 +0300

    Alter behavior when multiple grade objects are found for a student from applying last one, to applying the first

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,7 +5,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
     const studentWithGrade = { ...student };
     const studentGrade = newGrades.filter((grade) => grade.studentId === studentWithGrade.id);
-    studentWithGrade.grade = studentGrade[0] ? studentGrade.at(-1).grade : 'N/A';
+    studentWithGrade.grade = studentGrade[0] ? studentGrade[0].grade : 'N/A';
     return studentWithGrade;
   });
 }
