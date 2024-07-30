b7182917d10eacd3d61514af323fb8fdcbd27f41
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 19:05:32 +0300

    Try checking each array element with the has method of the set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,9 @@
 export default function hasValuesFromArray(set, array) {
   // Checks if every element of an array is in a set
-  return (set.isSupersetOf(new Set(array)));
+  for (const elem of array) {
+    if (!(set.has(elem))) {
+      return false;
+    }
+  }
+  return true;
 }
