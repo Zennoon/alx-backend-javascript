103ddadf88a883aa1c45df8258de883956de8fd7
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 18:45:54 +0300

    Check if the given position is not a number or is outside range

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const typedArray = new Int8Array(buffer);
+  if (typeof position !== 'number' || position >= buffer.byteLength) {
+    throw new Error('Position outside range');
+  }
   try {
     typedArray[position] = value;
   } catch (error) {
