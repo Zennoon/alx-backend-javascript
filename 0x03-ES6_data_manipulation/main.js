a191b5b61188114b078cb0a8bf271b0fdfb6249b
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-07-30 17:41:01 +0300

    Write a function that accepts an array of student objects, and a city name and returns an array of student objects with location equal to given city using the filter method

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,5 +1,6 @@
-import getListStudentIds from "./1-get_list_student_ids";
 import getListStudents from "./0-get_list_students";
+import getStudentsByLocation from "./2-get_students_by_loc";
 
-console.log(getListStudentIds("Hello"));
-console.log(getListStudentIds(getListStudents()));
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
