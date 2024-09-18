0fc56677ded3362e39871dfed9e6ebb49f741167
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 22:21:58 +0300

    Use node's http module to create a simple server with 2 defined endpoints

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,55 @@
+// A simple http server with 2 endpoints
+const { createServer } = require('http');
+const fs = require('fs');
+
+const dbPath = process.argv[2];
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      reject(Error('Cannot load the database'));
+    } else {
+      let returnString = '';
+      const studentsByField = {};
+      const lines = data.toString().trim().split('\n');
+      const records = lines.slice(1, lines.length);
+      let length = 0;
+
+      for (let i = 0; i < records.length; i += 1) {
+        const fields = records[i].split(',');
+        const fieldName = fields[3];
+        const studentName = fields[0];
+
+        length += 1;
+        if (fieldName in studentsByField) {
+          studentsByField[fieldName].push(studentName);
+        } else {
+          studentsByField[fieldName] = [studentName];
+        }
+      }
+
+      returnString += `Number of students: ${length}\n`;
+      for (const [key, val] of Object.entries(studentsByField)) {
+        if (key) {
+          returnString += `Number of students in ${key}: ${val.length}. List: ${val.join(', ')}\n`;
+        }
+      }
+      resolve(returnString.trim());
+    }
+  });
+});
+
+const app = createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(dbPath).then((data) => {
+      res.end(data);
+    });
+  }
+});
+
+app.listen(1245);
+module.exports = app;
