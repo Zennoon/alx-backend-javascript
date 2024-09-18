52df3fef9d94c4731f3abe8b596783372314e4b5
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 21:59:51 +0300

    Write a simple server using node's http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+// A simple server using Node's http module
+
+const { createServer } = require('http');
+
+const app = createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+})
+
+app.listen(1245);
