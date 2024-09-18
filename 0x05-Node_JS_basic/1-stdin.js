e5edba8171ac024aac67a6ced0aeb9d20c779b14
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 18:20:56 +0300

    Write a script that uses the process.stdin stream to read user input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name !== null) {
+    console.log(`Your name is ${name.trim()}`);
+  }
+});
+
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
