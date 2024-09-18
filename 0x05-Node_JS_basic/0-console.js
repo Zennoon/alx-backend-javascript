4a68005caec9c409da5e984d006fe7e6329c8b30
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 15:13:57 +0300

    Write a function that displays string argument to stdout

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Function that prints given string arg to STDOUT
+const displayMessage = (message) => {
+    process.stdout.write(message);
+}
+
+module.exports = displayMessage;
