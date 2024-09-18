18dfb232454ef50be8b4c458282c115bf9e31db6
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 19:43:35 +0300

    Insert all logic inside the try block

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,40 +7,40 @@ const countStudents = (path) => {
   let data;
 
   try {
-    data = fs.readFileSync(path).toString();
+    const data = fs.readFileSync(path).toString();
+
+    const lines = data.trim().split('\n');
+    const records = lines.slice(1, lines.length);
+
+    process.stdout.write(`Number of students: ${records.length}\n`);
+    const deptInfo = {};
+
+    for (const studentRecord of records) {
+      const fields = studentRecord.split(',');
+      const fName = fields[0];
+      const dept = fields[3];
+
+      if (dept in deptInfo) {
+        deptInfo[dept].studentCount += 1;
+        deptInfo[dept].studentNames.push(fName);
+      } else {
+        deptInfo[dept] = {
+          studentCount: 1,
+          studentNames: [fName],
+        };
+      }
+    }
+
+    for (const dept in deptInfo) {
+      if (dept) {
+        const count = deptInfo[dept].studentCount;
+        const allNames = deptInfo[dept].studentNames.join(', ');
+
+        process.stdout.write(`Number of students in ${dept}: ${count}. List: ${allNames}\n`);
+      }
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
-  }
-
-  const lines = data.trim().split('\n');
-  const records = lines.slice(1, lines.length);
-
-  process.stdout.write(`Number of students: ${records.length}\n`);
-  const deptInfo = {};
-
-  for (const studentRecord of records) {
-    const fields = studentRecord.split(',');
-    const fName = fields[0];
-    const dept = fields[3];
-
-    if (dept in deptInfo) {
-      deptInfo[dept].studentCount += 1;
-      deptInfo[dept].studentNames.push(fName);
-    } else {
-      deptInfo[dept] = {
-        studentCount: 1,
-        studentNames: [fName],
-      };
-    }
-  }
-
-  for (const dept in deptInfo) {
-    if (dept) {
-      const count = deptInfo[dept].studentCount;
-      const allNames = deptInfo[dept].studentNames.join(', ');
-
-      process.stdout.write(`Number of students in ${dept}: ${count}. List: ${allNames}\n`);
-    }
   }
 };
 
