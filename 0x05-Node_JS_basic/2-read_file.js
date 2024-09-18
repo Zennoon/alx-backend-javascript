b8c5fcbeb9c33a44f0a249510f3b581ceb7b68d9
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-18 21:14:11 +0300

    Check fr empty lines

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,23 +3,25 @@ const fs = require('node:fs');
 // Function attempts to read file with provided
 // path synchronously
 const countStudents = (path) => {
+  const studentsByField = {};
   try {
     const data = fs.readFileSync(path, 'utf-8').toString();
     const lines = data.trim().split('\n');
     const records = lines.slice(1, lines.length);
     let length = 0;
 
-    const studentsByField = {};
     for (let i = 0; i < records.length; i += 1) {
-      const recordFields = records[i].split(',');
-      const fieldName = recordFields[3];
-      const studentName = recordFields[0];
+      if (records[i]) {
+        const recordFields = records[i].split(',');
+        const fieldName = recordFields[3];
+        const studentName = recordFields[0];
 
-      length += 1;
-      if (Object.prototype.hasOwnProperty.call(studentsByField, fieldName)) {
-        studentsByField[fieldName].push(studentName);
-      } else {
-        studentsByField[fieldName] = [studentName];
+        length += 1;
+        if (Object.prototype.hasOwnProperty.call(studentsByField, fieldName)) {
+          studentsByField[fieldName].push(studentName);
+        } else {
+          studentsByField[fieldName] = [studentName];
+        }
       }
     }
     console.log(`Number of students: ${length}`);
@@ -30,7 +32,7 @@ const countStudents = (path) => {
       }
     }
   } catch (err) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
   }
 };
 
