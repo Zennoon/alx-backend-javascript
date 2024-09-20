f53f57538eb36fe6b2336356cfa238794fb8a2ea
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-21 00:36:24 +0300

    Fix typo in variable name

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -10,15 +10,15 @@ app.get('/cart/:id([0-9]+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-app.get('/available_payments', (request, response) => {
-  response.set("Content-Type", "application/json");
+app.get('/available_payments', (req, res) => {
+  res.set("Content-Type", "application/json");
   const paymentMethods = {
     payment_methods: {
       credit_cards: true,
       paypal: false
     }
   }
-  response.send(payMethods);
+  res.send(paymentMethods);
 });
 
 app.post('/login', (request, response) => {
