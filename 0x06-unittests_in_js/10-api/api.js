3c65ffaa281cad31c49f265cdcdc0c9a683724b4
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-21 17:57:48 +0300

    Fix the express app to read json arguments and write more appropriate tests

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
@@ -11,24 +13,23 @@ app.get('/cart/:id([0-9]+)', (req, res) => {
 });
 
 app.get('/available_payments', (req, res) => {
-  res.set("Content-Type", "application/json");
+  res.set('Content-Type', 'application/json');
   const paymentMethods = {
     payment_methods: {
       credit_cards: true,
       paypal: false
     }
-  }
+  };
   res.send(paymentMethods);
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.userName;
-  
   res.send(`Welcome ${username}`);
 });
 
 app.listen(7865, () => {
-    console.log('API available on localhost port 7865');
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,44 +2,126 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const request = require('request');
 
-describe('IndexRoute', function() {
-  it('should return 200 status code', function(done) {
-	  request('http://localhost:7865/', function(err, res) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-	  });
+describe('IndexRoute', function () {
+  it('should return 200 status code', function (done) {
+    request('http://localhost:7865/', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
-  it('should return correct output', function(done) {
-	  request('http://localhost:7865/', function(err, res, body) {
-	    expect(body).to.contain('Welcome to the payment system');
-	    done();
-  	});
+
+  it('should return correct output', function (done) {
+    request('http://localhost:7865/', function (err, res, body) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(body).to.contain('Welcome to the payment system');
+      done();
+    });
   });
-  it('should contain correct header', function(done) {
-	  request('http://localhost:7865/', function(err, res) {
-	    expect(res.headers['content-length']).to.equal('29');
-	    done();
-	  });
+
+  it('should contain correct header', function (done) {
+    request('http://localhost:7865/', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.headers['content-length']).to.equal('29');
+      done();
+    });
   });
 });
 
-describe('CartRoute', function() {
-  it('should return 200 status code', function(done) {
-	  request.get('http://localhost:7865/cart/1', function(err, res) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-	  });
+describe('CartRoute', function () {
+  it('should return 200 status code', function (done) {
+    request.get('http://localhost:7865/cart/1', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
-  it('should return correct output', function(done) {
-	  request.get('http://localhost:7865/cart/1', function(err, res, body) {
-	    expect(body).to.contain('Payment methods for cart 1');
-	    done();
-	  });
+
+  it('should return correct output', function (done) {
+    request.get('http://localhost:7865/cart/1', function (err, res, body) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(body).to.contain('Payment methods for cart 1');
+      done();
+    });
   });
-  it('should return 404 (since invalid param given)', function(done) {
-	  request.get('http://localhost:7865/cart/notNum', function(err, res) {
-	    expect(res.statusCode).to.equal(404);
-	    done();
-	  });
+
+  it('should return 404 (since invalid param given)', function (done) {
+    request.get('http://localhost:7865/cart/notNum', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('Login', function () {
+  const reqConfig = {
+    json: true,
+    body: {
+      userName: 'Yunus'
+    }
+  };
+  it('should return 200 status code', function (done) {
+    request.post('http://localhost:7865/login', reqConfig, function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('should return correct output', function (done) {
+    request.post('http://localhost:7865/login', reqConfig, function (err, res, body) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(body).to.contain('Welcome Yunus');
+      done();
+    });
+  });
+  // it('should return 404 (since invalid param given)', function (done) {
+  //   request.get('http://localhost:7865/cart/notNum', function (err, res) {
+  //     expect(res.statusCode).to.equal(404);
+  //     done();
+  //   });
+  // });
+});
+
+describe('Available Payment methods', function () {
+  it('should return the correct status code', function (done) {
+    request.get('http://localhost:7865/available_payments', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('should return the correct content', function (done) {
+    request.get('http://localhost:7865/available_payments', function (err, res) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      }
+      const json = JSON.parse(res.body);
+      expect(json).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+      done();
+    });
   });
 });
